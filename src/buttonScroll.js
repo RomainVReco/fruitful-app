05eb011dd5fdeceb2488a94bd157d9a64707bbdc
RomainV <romain.vorler@gmail.com>
2024-03-13 16:46:37 +0100

    2nd commit

---
+++ src/buttonScroll.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function ScrollToTopButton() {
+
+  return (
+    <div>
+      <button type="submit">Scroll to Top</button>
+      Coucou
+    </div>
+  );
+}
+
+export default ScrollToTopButton;
