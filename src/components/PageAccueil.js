b81aa71d4cdf31d1978b9b48fd9c2a8b7d6ac09c
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-03-14 16:29:38 +0100

    Creation d un bandeau violet

--- src/components/PageAccueil.js
+++ src/components/PageAccueil.js
@@ -1,10 +1,42 @@
 import React from "react";
 
-function PageAccueil () {
+import PurpleRectangle from './PurpleRectangle';
+
+import GenericButton from './GenericButton'
+import Tache from './Tache';
+
+
+export default function PageAccueil () {
     
     return (
-        <div>Hello World !</div>
-    )
+
+    <div className="PageAccueil">
+      <PurpleRectangle />
+      {/* Autres composants ou contenu ici */}
+    </div>
+  );
 }
 
-export default PageAccueil;
+
+//export default function App() {
+//export default App;
+
+    /*
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <GenericButton label={'Valider'} buttonStyle={'boutonValider'} method={handleSubmit}/>
+      <GenericButton label={'Annuler'} buttonStyle={'boutonAnnuler'} method={handleSubmit}/>
+      <GenericButton label={'Valider'} buttonStyle={'boutonValider'} method={handleSubmit}/>
+      <Tache nomHabitude="Lire un livre" quota='20' quantiteQuota='pages' heure='10h30' />
+      <Tache nomHabitude="Lire un livre" quota='20' quantiteQuota='pages' heure='10h45' />
+    </div>
+
+    )
+}
+*/
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log('Bouton cliqué')
+  }
