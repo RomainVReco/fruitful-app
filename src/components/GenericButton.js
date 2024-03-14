63ce690af6e43a0b5ae6e73d69c0a6d14fe4409e
RomainV <romain.vorler@gmail.com>
2024-03-14 12:20:26 +0100

    ajout méthode

--- src/components/GenericButton.js
+++ src/components/GenericButton.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Button } from 'react-bootstrap';
 
-function GenericButton ({label, buttonStyle}) {
+function GenericButton ({label, buttonStyle, method}) {
 
     return (
         <div>
-            <button type="submit" className={buttonStyle}>{label}</button>
+            <button type="submit" className={buttonStyle} onClick={method}>{label}</button>
         </div>
     )
 }
