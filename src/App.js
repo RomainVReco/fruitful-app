43d3db83d747858eed5027722778b0cab4423632
RomainV <romain.vorler@gmail.com>
2024-03-14 09:52:48 +0100

    ajout bouton generic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import GenericButton from './components/GenericButton';
+
+const handleSubmit = () => {
+  console.log('Bouton cliqué')
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <GenericButton label={'Valider'} buttonStyle={'boutonValider'}/>
+      <GenericButton label={'Annuler'} buttonStyle={'boutonAnnuler'}/>
     </div>
   );
 }
