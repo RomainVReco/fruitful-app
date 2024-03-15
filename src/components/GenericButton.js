a5c34ee26f3334e0e4ac3f9525be87c92ceed9f0
RomainV <romain.vorler@gmail.com>
2024-03-15 12:22:58 +0100

    mep bouton action bar et alignement taches

--- src/components/GenericButton.js
+++ src/components/GenericButton.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function GenericButton ({label, buttonStyle, method}) {
 
     return (
         <div>
-            <button type="submit" className={buttonStyle} onClick={method}>{label}</button>
+            <button type="submit" className={buttonStyle}  onClick={method}>{label}</button>
         </div>
     )
 }
