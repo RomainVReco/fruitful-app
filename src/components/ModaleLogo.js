d4cdef1a699ddff7c1d8e8de5bf3c88afb416385
Romain VORLET <romain.vorlet@gmail.com>
2024-03-16 15:27:33 +0100

    tableau overlay

--- src/components/ModaleLogo.js
+++ src/components/ModaleLogo.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react'
 import { createPortal } from 'react-dom'
 
+const tableauIcones = {"data":[{"image":'titre', titre:'image'}, ]}
 
 export default function ModaleLogo({open, children, onClose}) {
 
@@ -12,9 +13,25 @@ export default function ModaleLogo({open, children, onClose}) {
     <>
     <div className='overlay-style'/>
     <div className='modale-basique'>
+      <h5>Sélectionnez une illustration pour votre nouvelle habitude :</h5>
+      <div className='container d-flex justify-content-evenly'>
+        {tableauIcones.map((icone, index) => (
+          <div key={index} className='d-flex flex-column justify-content-center' onClick={handleClickOnIcon}>
+            <div>{icone.image}</div>
+            <div>{icone.titre}</div>
+          </div>
+
+        ))}
+
+
+        <div>
+          <h5 className='card-subtitle'>Avion</h5>
+        </div>
+      </div>
       
 
       <button onClick={onClose}>Fermer</button>
+
         {children}
     </div>
     </>
