66109de9da596a1715b276c8817e92b7c6f34d55
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-03-19 22:05:01 +0100

    centrage des images

--- src/components/ImageAccueil.js
+++ src/components/ImageAccueil.js
@@ -3,27 +3,29 @@ import Image from 'react-bootstrap/Image';
 
 function ImageAccueil({ src, alt, brandText, paragraphText }) {
   return (
-    <nav className="navbar bg-body-tertiary rounded-pi11">
-      <div className="container-fluid d-flex justify-content-center align-items-center">
-        <a className="navbar-brand" href="#">
-          <Image src={src} rounded alt={alt} className="img" style={{ maxWidth: '50%' }} />
-          {brandText}
-        </a>
-      </div>
-      <div className="container-fluid text-center">
-        <div className="row">
-          {/*<div className="col-12 text-center"> */}
-            {/*<p className="text-center">{paragraphText}</p> */}
+    <div className="container-fluid d-flex justify-content-center align-items-center">
+        <nav className="navbar bg-body-tertiary rounded-pi11">
+            <div className="container-fluid d-flex justify-content-center align-items-center">
+                <a className="navbar-brand" href="#">
+                <Image src={src} rounded alt={alt} className="img" style={{ maxWidth: '100%' }} />
+                {brandText}
+                </a>
+            </div>
+            <div className="container-fluid text-center">
+                <div className="row">
+                {/*<div className="col-12 text-center"> */}
+                    {/*<p className="text-center">{paragraphText}</p> */}
 
-        </div>
-      </div>
+                </div>
+            </div>
 
-      <div className="container-fluid d-flex justify-content-center align-items-center">
-        <h1>
-            {paragraphText}
-        </h1>
-      </div>
-    </nav>
+            <div className="container-fluid d-flex justify-content-center align-items-center">
+                <h1>
+                    {paragraphText}
+                </h1>
+            </div>
+        </nav>
+    </div>
   );
 }
 
