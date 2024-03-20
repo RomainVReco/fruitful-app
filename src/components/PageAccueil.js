fa2a6d241197969aae84dff03f7a99424efb7c6a
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-03-20 10:42:13 +0100

    Limitation largeur Carousel

--- src/components/PageAccueil.js
+++ src/components/PageAccueil.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import Header from './Header';
 import ImageAccueil from './ImageAccueil'
 import Carousel from './Carousel'
 import logo1 from '../assets/tournesol.png';
 import logo2 from '../assets/Ciel_Bleu_Mieux_Vivre.png';
 import logo3 from '../assets/fleurs_illustre_mieux_gerer_sa_vie.png';
 import logo4 from '../assets/ciel_nuageux_Mieux_gerer_ses_addictions.png';
-import BankInfoForm from './BankInfoForm';
 
 export default function PageAccueil () {
     
@@ -21,8 +19,9 @@ export default function PageAccueil () {
         <br />
         <br />
 
-
-        <Carousel />
+        <div className="carousel-container">
+                <Carousel />
+        </div>
 
         <br />
         <br />
@@ -30,19 +29,15 @@ export default function PageAccueil () {
         <br />
 
         <ImageAccueil src={logo2} alt="image du ciel bleu" brandText="" paragraphText="Mieux être" />
-        <div>
 
         <br />
 
-        </div>
             <ImageAccueil src={logo3} alt="image de fleurs" brandText="" paragraphText="Mieux gérer sa vie" />
-        {/*</div> */}
 
         <br />
-        
+
         <ImageAccueil src={logo4} alt="image de fleurs" brandText="" paragraphText="Se libérer de ses addictions" />
-        { /*<BankInfoForm /> */}
-        {/* Autres composants ou contenu ici */}
+ 
     </div>
   );
 }
