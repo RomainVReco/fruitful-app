4be2c0677ad1e0e5f1ec54c2462f0c717604e45e
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-03-20 22:11:23 +0100

    ajout de 2 autres landingPages

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Base from './components/Base';
 import CreationTache from './components/CreationTache';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import PageLandingPage from './components/PageLandingPage';
+import PageLandingPage2 from './components/PageLandingPage2';
+import PageLandingPage3 from './components/PageLandingPage3';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -20,6 +22,8 @@ function App() {
       <Routes>
         <Route path ="/pageAccueil" element={<PageAccueil/>} />
         <Route path ="/pageLandingPage" element={<PageLandingPage/>} />
+        <Route path ="/pageLandingPage2" element={<PageLandingPage2/>} />
+        <Route path ="/pageLandingPage3" element={<PageLandingPage3/>} />
         <Route path ="/pageInscriptionPremium" element={<PageInscriptionPremium/>} />
         <Route path = "/" element={<Base/>} />
         <Route path = "/creationTache" element={<CreationTache/>} />
---
+++ src/components/BoutonAccesEER.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+//Fonction concernant le Bouton d'Acces à l'Entrée En Relation (EER) avec l'utilisateur (questionnaire pour mieux le connaitre)
+function BoutonAccesEER() {
+  const buttonStyle = {
+      fontSize: '2rem',
+      padding: '30px 40px',
+      margin: '50px',
+    };
+
+  return (
+      <Container fluid className="d-flex justify-content-center align-items-center vh-30">
+      <Row>
+        <Col className="text-center">
+          <Link to="/pageInscriptionPremium">
+            <Button variant="warning" style={buttonStyle}>Je rejoins l'aventure!</Button>
+          </Link>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default BoutonAccesEER;
+
+
+
