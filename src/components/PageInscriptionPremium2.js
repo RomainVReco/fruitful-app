7d0f5b4d1dceec337c941e46d3e6fa8584e2b7a6
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-03-24 16:54:05 +0100

    ajout de la somme à payer

--- src/components/PageInscriptionPremium2.js
+++ src/components/PageInscriptionPremium2.js
@@ -3,12 +3,37 @@ import React, { useEffect } from 'react';
 const InscriptionPremium = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://www.paypal.com/sdk/js?client-id=AUc9A_Q4BlFKrhLXmVpRkDP4DChqyk3mXCqw_v23Kluwo891aTJRXDhuzLWkAdM6042ih1aGG37s9Kwu';
+    script.src = 'https://www.paypal.com/sdk/js?client-id=AUc9A_Q4BlFKrhLXmVpRkDP4DChqyk3mXCqw_v23Kluwo891aTJRXDhuzLWkAdM6042ih1aGG37s9Kwu'; // Assurez-vous de remplacer YOUR_SANDBOX_CLIENT_ID par votre propre identifiant client sandbox PayPal
     script.addEventListener('load', () => {
       // Le script PayPal est chargé, nous pouvons maintenant utiliser ses fonctionnalités
-      /* global paypal */ // Ce commentaire indique à ESLint que paypal est une variable globale
-      paypal.Buttons().render("#paypal-button-container");
+      window.paypal.Buttons({
+        createOrder: function(data, actions) {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: '0.01'
+                }
+              }
+            ]
+          });
+        },
+        onApprove: function(data, actions) {
+          return actions.order.capture().then(function(details) {
+            alert("Transaction réalisée : " + details.payer.name.given_name);
+          });
+        },
+        onError: function(err) {
+          console.error('Payment Error :', err);
+          alert("Paiement a échoué!");
+        }
+      }).render("#paypal-button-container");
     });
+
+    script.onerror = function() {
+        console.error("Erreur lors du chargement du script PayPal.");
+    };
+
     document.body.appendChild(script);
 
     return () => {
