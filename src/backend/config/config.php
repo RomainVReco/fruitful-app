48440507c7057f11d6916de507bf3deef2c186c7
Romain VORLET <romain.vorlet@gmail.com>
2024-03-28 22:57:43 +0100

    modification .gitignore

--- src/backend/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-try {
-    $mysqlClient = new PDO('mysql:host=localhost;dbname=fruitful;port=3306;charset=utf8mb4', 'root', '');} catch (Exception $exception) {
-    die('Erreur : ' . $exception->getMessage());
-}
