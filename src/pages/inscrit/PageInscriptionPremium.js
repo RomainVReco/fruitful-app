9fe2ea6bb374d78ff91464e8a46653f8ac5b1507
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-03-29 12:17:02 +0100

    ajout try/catch pb cnx API

--- src/pages/inscrit/PageInscriptionPremium.js
+++ src/pages/inscrit/PageInscriptionPremium.js
@@ -11,46 +11,51 @@ const InscriptionPremium = () => {
   const [transactionMessage, setTransactionMessage] = useState('');
 
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://www.paypal.com/sdk/js?client-id=AUc9A_Q4BlFKrhLXmVpRkDP4DChqyk3mXCqw_v23Kluwo891aTJRXDhuzLWkAdM6042ih1aGG37s9Kwu'; //votre propre identifiant client PayPal
-    script.addEventListener('load', () => {
-      // Le script PayPal est chargé, nous pouvons maintenant utiliser ses fonctionnalités
-      window.paypal.Buttons({
-        createOrder: function(data, actions) {
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: '0.01' // Montant de la transaction
+    try {
+      const script = document.createElement('script');
+      script.src = 'https://www.paypal.com/sdk/js?client-id=AUc9A_Q4BlFKrhLXmVpRkDP4DChqyk3mXCqw_v23Kluwo891aTJRXDhuzLWkAdM6042ih1aGG37s9Kwu'; //votre propre identifiant client PayPal
+      script.addEventListener('load', () => {
+        // Le script PayPal est chargé, nous pouvons maintenant utiliser ses fonctionnalités
+        window.paypal.Buttons({
+          createOrder: function(data, actions) {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: '0.01' // Montant de la transaction
+                  }
                 }
-              }
-            ]
-          });
-        },
-        onApprove: function(data, actions) {
-          return actions.order.capture().then(function(details) {
-            const message = `Bravo ${details.payer.name.given_name}! Transaction validée par PayPal. Accès Fruitful Premium accordé`;
-            setTransactionMessage(message); // Mettre à jour le message de transaction
-            setFirstModalShow(true); // Ouvrir la première modale
-          });
-        },
-        onError: function(err) {
-          console.error('Payment Error :', err);
-          alert("Paiement a échoué!");
-        }
-      }).render("#paypal-button-container");
-    });
+              ]
+            });
+          },
+          onApprove: function(data, actions) {
+            return actions.order.capture().then(function(details) {
+              const message = `Bravo ${details.payer.name.given_name}! Transaction validée par PayPal. Accès Fruitful Premium accordé`;
+              setTransactionMessage(message); // Mettre à jour le message de transaction
+              setFirstModalShow(true); // Ouvrir la première modale
+            });
+          },
+          onError: function(err) {
+            console.error('Payment Error :', err);
+            alert("Paiement a échoué!");
+          }
+        }).render("#paypal-button-container");
+      });
 
-    script.onerror = function() {
-      console.error("Erreur lors du chargement du script PayPal.");
-    };
+      script.onerror = function() {
+        console.error("Erreur lors du chargement du script PayPal.");
+      };
 
-    document.body.appendChild(script);
+      document.body.appendChild(script);
 
-    return () => {
-      // Nettoyer le script lors du démontage du composant
-      document.body.removeChild(script);
-    };
+      return () => {
+        // Nettoyer le script lors du démontage du composant
+        document.body.removeChild(script);
+      };
+    } catch (error) {
+      console.error("Erreur lors de l'appel à l'API PayPal :", error);
+      alert("Une erreur est survenue lors de la communication avec PayPal.");
+    }
   }, []);
 
   const handleCloseFirstModal = () => {
