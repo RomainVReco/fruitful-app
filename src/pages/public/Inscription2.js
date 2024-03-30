e4ec0d15dbf9b89e9b269814e8d9a322e7681626
bidibul75 <walter.bianchi@edu.esiee-it.fr>
2024-03-30 04:14:25 +0100

    Tel si utilisateur existe en base (email) : si oui : on propose un login, si non on créée l'utilisateur en base.

--- src/pages/public/Inscription2.js
+++ src/pages/public/Inscription2.js
@@ -11,6 +11,11 @@ import {
 } from "react-router-dom";
 
 export default function Inscription2() {
+  function formatDate(date) {
+    const [jour, mois, annee] = date.split('/');
+    return `${annee}-${mois}-${jour}`;
+  }
+
   const [dateNaissance, setDate] = useState("");
   const [messageErreur, setMessageErreur] = useState("");
 
@@ -23,17 +28,19 @@ export default function Inscription2() {
 
   const handleClick = () => {
     const regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-    let formatAnnee=4;
+    let formatAnnee = 4;
     let traitementDate = dateNaissance.split("/");
-    if (traitementDate[2].length != 4) formatAnnee=2;
+    if (traitementDate[2].length != 4) formatAnnee = 2;
 
-    if (!dateNaissance.match(regex) || formatAnnee!=4) {
+    if (!dateNaissance.match(regex) || formatAnnee != 4) {
       setMessageErreur(
         "La date doit être au format 01/01/1970 et doit être valide."
       );
     } else {
       setMessageErreur(""); // Effacer le message d'erreur s'il n'y a pas d'erreur    
-      sessionStorage.setItem("dateNaissance", dateNaissance);
+      // Fonction pour reformater la date au format YYYY-MM-DD
+      let dateNaissanceFormatSQL = formatDate(dateNaissance);
+      sessionStorage.setItem("dateNaissance", dateNaissanceFormatSQL);
       navigate(url);
 
       return;
