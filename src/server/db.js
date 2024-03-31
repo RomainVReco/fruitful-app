4388188b6cf0ca597f63f7db9d25a6ef98bb981f
bidibul75 <walter.bianchi@edu.esiee-it.fr>
2024-03-31 19:17:00 +0200

    Correction accolades et parenthèses suite au merge.

--- src/server/db.js
+++ src/server/db.js
@@ -1,31 +1,31 @@
-    import express from 'express'
-    import mysql from 'mysql'
-    import cors from 'cors'
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors'
 
-    const PORT = 8081
+const PORT = 8081
 
-    const app = express()
-    app.use(cors())
-    app.use(express.json())
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-    const db = mysql.createConnection({
-        port: "3306",
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "fruitful"
+const db = mysql.createConnection({
+    port: "3306",
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "fruitful"
+})
+
+
+// ************** UTILISATEUR ****************** //
+/* Méthodes CRUD pour les utilisateurs */
+
+app.get('/getAllUsers', (req, res) => {
+    const sql = "SELECT * FROM utilisateur"
+    db.query(sql, (err, result) => {
+        if (err) return res.json({ Message: "Error in Node" })
+        return res.json(result)
     })
-
-
-    // ************** UTILISATEUR ****************** //
-    /* Méthodes CRUD pour les utilisateurs */
-
-    app.get('/getAllUsers', (req, res) => {
-        const sql = "SELECT * FROM utilisateur"
-        db.query(sql, (err, result) => {
-            if (err) return res.json({ Message: "Error in Node" })
-            return res.json(result)
-        })
 
 })
 
@@ -78,27 +78,28 @@ app.put('/updateClient', (req, res) => {
         }
 
     })
+})
 
-    app.put('/updateClient', (req, res) => {
-        const sql = "UPDATE utilisateur SET nom = ?, prenom = ?, email = ?, newsletter = ?, specialOffer = ? WHERE idUtilisateur = ? ;"
-        db.query(sql, [req.body.nom, req.body.prenom, req.body.email, req.body.newsletter, req.body.specialOffer, req.body.idUtilisateur], (err, data) => {
-            if (err) {
-                console.log("Echec de l'exécution de la requête de mise à jours des informations clients : ", err)
-                return res.status(500).json("Echec de l'exécution de la requête de mise à jours des informations clients : " + err)
-            }
-            if (data.affectedRows > 0) {
-                console.log("Informations clients mises à jour avec succès.");
-                return res.status(200).json({ success: "Informations clients mises à jour avec succès.", data: data });
-            } else {
-                console.log("Aucun client trouvé avec l'ID spécifié.");
-                return res.status(404).json({ error: "Aucun client trouvé avec l'ID spécifié." });
-            }
-        })
-
+app.put('/updateClient', (req, res) => {
+    const sql = "UPDATE utilisateur SET nom = ?, prenom = ?, email = ?, newsletter = ?, specialOffer = ? WHERE idUtilisateur = ? ;"
+    db.query(sql, [req.body.nom, req.body.prenom, req.body.email, req.body.newsletter, req.body.specialOffer, req.body.idUtilisateur], (err, data) => {
+        if (err) {
+            console.log("Echec de l'exécution de la requête de mise à jours des informations clients : ", err)
+            return res.status(500).json("Echec de l'exécution de la requête de mise à jours des informations clients : " + err)
+        }
+        if (data.affectedRows > 0) {
+            console.log("Informations clients mises à jour avec succès.");
+            return res.status(200).json({ success: "Informations clients mises à jour avec succès.", data: data });
+        } else {
+            console.log("Aucun client trouvé avec l'ID spécifié.");
+            return res.status(404).json({ error: "Aucun client trouvé avec l'ID spécifié." });
+        }
     })
 
-    // ************** SOUSCRIPTION ****************** //
-    /* Méthodes pour souscription (abonnement Premium) */
+})
+
+// ************** SOUSCRIPTION ****************** //
+/* Méthodes pour souscription (abonnement Premium) */
 
 app.post('/updateSubscription/:id', (req, res) => {
     const idUtilisateur = req.params.id;
@@ -151,36 +152,37 @@ app.post('/createAddress', (req, res) => {
                     console.log("Error updating utilisateur table:", utilisateurErr);
                     return res.status(500).json({ error: "Error updating utilisateur table", details: utilisateurErr });
                 }
-                )
-            } else {
-                return res.json("Comportement inattendu lors de la création de la nouvelle adresse")
-            }
-        })
+                else {
+                    return res.json("Comportement inattendu lors de la création de la nouvelle adresse")
+                }
+            })
+        }
     })
-
-    app.put('/updateAddress', (req, res) => {
-        console.log("updateAddress")
-        console.log("type adresse : ", typeof (req.body.adresse))
-        console.log("type codePostal : ", typeof (req.body.codePostal))
-        console.log("type ville : ", typeof (req.body.ville))
-        console.log("type idAdresse : ", typeof (req.body.idAdresse))
-        const sql = "UPDATE adresse SET adresse = ?, codePostal = ?, ville = ? WHERE idAdresse = ?"
-        db.query(sql, [req.body.adresse, req.body.codePostal, req.body.ville, req.body.idAdresse], (err, data) => {
-            if (err) {
-                console.log("Erreur lors de l'exécution de la requête de mise à jour de l'adresse :", err);
-                return res.status(500).json({ error: "Erreur lors de l'exécution de la requête de mise à jour de l'adresse.", details: err });
-            }
-
-            if (data && data.affectedRows > 0) {
-                return res.status(200).json({ success: "Adresse mise à jour avec succès.", data: data });
-            } else {
-                console.log(res);
-                return res.status(404).json({ error: "Aucune adresse trouvée avec l'ID spécifié." });
-            }
-        })
-    })
-
 })
+
+app.put('/updateAddress', (req, res) => {
+    console.log("updateAddress")
+    console.log("type adresse : ", typeof (req.body.adresse))
+    console.log("type codePostal : ", typeof (req.body.codePostal))
+    console.log("type ville : ", typeof (req.body.ville))
+    console.log("type idAdresse : ", typeof (req.body.idAdresse))
+    const sql = "UPDATE adresse SET adresse = ?, codePostal = ?, ville = ? WHERE idAdresse = ?"
+    db.query(sql, [req.body.adresse, req.body.codePostal, req.body.ville, req.body.idAdresse], (err, data) => {
+        if (err) {
+            console.log("Erreur lors de l'exécution de la requête de mise à jour de l'adresse :", err);
+            return res.status(500).json({ error: "Erreur lors de l'exécution de la requête de mise à jour de l'adresse.", details: err });
+        }
+
+        if (data && data.affectedRows > 0) {
+            return res.status(200).json({ success: "Adresse mise à jour avec succès.", data: data });
+        } else {
+            console.log(res);
+            return res.status(404).json({ error: "Aucune adresse trouvée avec l'ID spécifié." });
+        }
+    })
+})
+
+
 
 // ************** TÂCHES ****************** //
 /* Méthodes pour la création de tâche */
@@ -291,9 +293,9 @@ app.put('/updateEventStatus/:idEvenement', (req, res) => {
 app.get('/getAllUserEvents/:idUtilisateur', (req, res) => {
     const idUtilisateur = req.params.idUtilisateur
     console.log("getAllUserEvents : ", idUtilisateur)
-    const sql = "SELECT idEvenement, nomEvenement, dateDebut, frequence, eve.idTypeEvenement, t_e.nomTypeEvenement, idIcone "+
-    "FROM evenement as eve INNER JOIN type_evenement as t_e ON eve.idTypeEvenement = t_e.idTypeEvenement "+
-    "WHERE eve.idUtilisateur = ? AND eve.estActif = 1;"
+    const sql = "SELECT idEvenement, nomEvenement, dateDebut, frequence, eve.idTypeEvenement, t_e.nomTypeEvenement, idIcone " +
+        "FROM evenement as eve INNER JOIN type_evenement as t_e ON eve.idTypeEvenement = t_e.idTypeEvenement " +
+        "WHERE eve.idUtilisateur = ? AND eve.estActif = 1;"
     db.query(sql, [idUtilisateur], (err, data) => {
         if (err) {
             return res.status(500).json({
@@ -301,7 +303,7 @@ app.get('/getAllUserEvents/:idUtilisateur', (req, res) => {
                 details: err
             })
         }
-        if (data && data.length> 0) {
+        if (data && data.length > 0) {
             return res.status(200).json({ success: "Liste des évènements récupérés", resultat: data })
         } else {
             return res.status(404).json({ failure: "Aucun évènement trouvé pour ce client", resultat: data })
@@ -312,8 +314,8 @@ app.get('/getAllUserEvents/:idUtilisateur', (req, res) => {
 app.get('/getEvent/:idEvenement', (req, res) => {
     const idEvenement = req.params.idEvenement
     console.log('getEvent idEvenement : ', idEvenement)
-    const sql = "SELECT idEvenement, nomEvenement as nom, dateDebut, frequence, idTypeEvenement as typeEvenement, "+
-    "idIcone as logo FROM evenement WHERE idEvenement = ?;"
+    const sql = "SELECT idEvenement, nomEvenement as nom, dateDebut, frequence, idTypeEvenement as typeEvenement, " +
+        "idIcone as logo FROM evenement WHERE idEvenement = ?;"
     db.query(sql, [idEvenement], (err, data) => {
         if (err) {
             return res.status(500).json({
@@ -321,7 +323,7 @@ app.get('/getEvent/:idEvenement', (req, res) => {
                 details: err
             })
         }
-        if (data && data.length> 0) {
+        if (data && data.length > 0) {
             return res.status(200).json({ success: `Evenement ${idEvenement} récupéré avec succès`, resultat: data })
         } else {
             return res.status(404).json({ failure: "Aucun évènement trouvé", resultat: data })
@@ -333,46 +335,46 @@ app.listen(PORT, () => {
     console.log("Connected to the server")
 })
 
-    // ************** INSCRIPTION ****************** //
-    /* Méthodes CRUD pour l'inscription */
+// ************** INSCRIPTION ****************** //
+/* Méthodes CRUD pour l'inscription */
 
-    // Vérification si l'e-mail existe en BDD ou pas
-    app.post('/checkEmail', (req, res) => {
+// Vérification si l'e-mail existe en BDD ou pas
+app.post('/checkEmail', (req, res) => {
 
-        const email = req.body.email
-        console.log('server : ', email)
-        const sql = "SELECT idUtilisateur FROM utilisateur WHERE email = ?"
-        db.query(sql, [email], (error, data) => {
-            if (error) {
-                console.log("err : ", error);
-            }
-            console.log("data.length > 0 ", data.length > 0)
-            if (data.length > 0) {
-                console.log("data >0: ", data)
-                return res.status(200).json({ success: `success`, resultat: data })
-            } else {
-                console.log("data=0 : ", data)
-                return res.json({ success: `echec`, resultat: data })
-            }
-        })
-
+    const email = req.body.email
+    console.log('server : ', email)
+    const sql = "SELECT idUtilisateur FROM utilisateur WHERE email = ?"
+    db.query(sql, [email], (error, data) => {
+        if (error) {
+            console.log("err : ", error);
+        }
+        console.log("data.length > 0 ", data.length > 0)
+        if (data.length > 0) {
+            console.log("data >0: ", data)
+            return res.status(200).json({ success: `success`, resultat: data })
+        } else {
+            console.log("data=0 : ", data)
+            return res.json({ success: `echec`, resultat: data })
+        }
     })
 
-    // On écrit en base les renseignements fournis lors des 3 pages d'inscription.
-    app.put('/enregistrementInscription', (req, res) => {
-           const { nom, prenom, dateNaissance, email, password } = req.body
-        const sql = "INSERT INTO utilisateur (nom, prenom, dateNaissance, email, password) VALUES (?, ?, ?, ?, ?);"
-        db.query(sql, [nom, prenom, dateNaissance, email, password], (err, data) => {
-            if (err) {
-                return res.status(500).json({
-                    error: "Erreur lors de l'enregistrement d'un nouvel utilisateur.",
-                    details: err
-                })
-            }
-            if (data && data.affectedRows > 0) {
-                return res.status(200).json({ success: `Utilisateur ${nom} ajouté à la base avec succès`, resultat: data })
-            } else {
-                return res.json({ failure: `Utilisateur ${nom} non ajouté à la base`, resultat: data })
-            }
-        })
+})
+
+// On écrit en base les renseignements fournis lors des 3 pages d'inscription.
+app.put('/enregistrementInscription', (req, res) => {
+    const { nom, prenom, dateNaissance, email, password } = req.body
+    const sql = "INSERT INTO utilisateur (nom, prenom, dateNaissance, email, password) VALUES (?, ?, ?, ?, ?);"
+    db.query(sql, [nom, prenom, dateNaissance, email, password], (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                error: "Erreur lors de l'enregistrement d'un nouvel utilisateur.",
+                details: err
+            })
+        }
+        if (data && data.affectedRows > 0) {
+            return res.status(200).json({ success: `Utilisateur ${nom} ajouté à la base avec succès`, resultat: data })
+        } else {
+            return res.json({ failure: `Utilisateur ${nom} non ajouté à la base`, resultat: data })
+        }
     })
+})
