45d6b0c72cabffb3ef4ffdacc46e2536365a6a7e
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-04-04 12:40:45 +0200

    Repartition footer homogene 3 colonnes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,22 +9,24 @@ function Footer() {
   return (
 
     <footer className="footerBottom">
-      <div className="footer-container" >
-      <div className="left-column">
+      <div className="footer-container">
+        <div className="left-column">
           <div>
             <Link className="" to='/'>
               <img src={logoComplet} alt="logo fruitful" className="image-footer" />
             </Link>
           </div>
-          <div>
-            <p>&copy; {new Date().getFullYear()} Fruitful Coaching Numérique SARL. Tous droits réservés.</p>
-          </div>
         </div>
-        <div className="right-column">
+        <div className="center-column">
           <div className="vertical-footer-links">
             <Link className='lien' to="/contactezNous">Contactez-nous</Link>
             <Link className='lien' to="/PageQuiSommesNous">Qui sommes nous?</Link>
             <Link className='lien' to="/PageMentionsLegales">Mentions légales</Link>
+          </div>
+        </div>
+        <div className="right-column">
+          <div>
+            <p>&copy; {new Date().getFullYear()} Fruitful Coaching Numérique SARL. Tous droits réservés.</p>
           </div>
         </div>
       </div>
